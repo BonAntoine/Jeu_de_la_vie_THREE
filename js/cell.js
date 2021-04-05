5e7835a81f510fe7ac8eb796009f0d5336de5551
Antoine Bon <antoine.bon.etu@univ-lille.fr>
2021-04-05 10:53:55 +0200

    feat: add step done

--- js/cell.js
+++ js/cell.js
@@ -10,9 +10,13 @@ class Cell {
 	}
 
 	addToScene(scene) {
+		// + 0.5 to correct position on grid
 		this.mesh.translateX(this.x + 0.5);
 		this.mesh.translateY(this.y + 0.5);
+		// Z axe translate to match Z position of grid
 		this.mesh.translateZ(-0.5);
+		// Set name property to make it findable
+		this.mesh.name = this.x + "," + this.y;
 		return scene.add(this.mesh);
 	}
 }
--- js/main.js
+++ js/main.js
@@ -10,8 +10,8 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-const size = 80;
-const cellStartAmount = 120;
+const size = 40;
+const cellStartAmount = 190;
 
 let points = [];
 const material = new THREE.LineBasicMaterial({ color: 0x00ff00 });
@@ -62,8 +62,6 @@ for (let i = 0; i < cellStartAmount; i++) {
 
 console.log("FINISHED");
 
-
-
 renderer.render(scene, camera);
 
 camera.position.z = 100;
@@ -77,39 +75,75 @@ function gameLoop() {
 	renderer.render(scene, camera);
 
 	doStep();
-	updateDisplay();
+	// updateDisplay();
 }
 
-function updateDisplay() {
+window.addEventListener("keydown", function (event) {
+	console.log("a", event.key);
+	if (event.key === "Enter") {
+		console.log("updated !");
+		// doStep();
+		updateDisplay();
+	}
+});
 
+function updateDisplay() {
+	for (let i = 0; i <= size; i++) {
+		for (let j = 0; j <= size; j++) {
+			let currentCell = scene.getObjectByName(i + "," + j);
+			// console.log("--> ", currentCell);
+			if (currentCell) {
+				if (cellArray[i][j] === 0) {
+					scene.remove(currentCell);
+					// console.log("removed ! ", i, " ", j);
+				}
+			} else {
+				if (cellArray[i][j] === 1) {
+					const cube = new THREE.Mesh(geometryCube, materialCube);
+					let cell = new Cell(i, j, cube);
+					cell.addToScene(scene);
+					// console.log("added ! ", i, " ", j);
+				}
+			}
+		}
+	}
 }
 
 function getNeighbour(x, y) {
 	let neighbourNumber = 0;
 	if (x > 1) {
-		neighbourNumber += cellArray[x - 1, y];
+		neighbourNumber += cellArray[x - 1][y];
+		console.log(cellArray[x - 1][y]);
 	}
 	if (x < size) {
-		neighbourNumber += cellArray[x + 1, y];
+		neighbourNumber += cellArray[x + 1][y];
+		console.log(cellArray[x + 1][y])
 	}
 	if (y > 1) {
-		neighbourNumber += cellArray[x, y - 1];
+		neighbourNumber += cellArray[x][y - 1];
+		console.log(cellArray[x][y - 1]);
 	}
-	if (x < size) {
-		neighbourNumber += cellArray[x, y + 1];
+	if (y < size) {
+		neighbourNumber += cellArray[x][y + 1];
+		console.log(cellArray[x][y + 1]);
 	}
 	if (x > 1 && y > 1) {
-		neighbourNumber += cellArray[x - 1, y - 1];
+		neighbourNumber += cellArray[x - 1][y - 1];
+		console.log(cellArray[x - 1][y - 1]);
 	}
 	if (x > 1 && y < size) {
-		neighbourNumber += cellArray[x - 1, y + 1];
+		neighbourNumber += cellArray[x - 1][y + 1];
+		console.log(cellArray[x - 1][y + 1]);
 	}
 	if (x < size && y < size) {
-		neighbourNumber += cellArray[x + 1, y + 1];
+		neighbourNumber += cellArray[x + 1][y + 1];
+		console.log(cellArray[x + 1][y + 1]);
 	}
 	if (x < size && y > 1) {
-		neighbourNumber += cellArray[x + 1, y - 1];
+		neighbourNumber += cellArray[x + 1][y - 1];
+		console.log(cellArray[x + 1][y - 1]);
 	}
+	console.log("result = ", neighbourNumber);
 	return neighbourNumber;
 }
 
@@ -119,10 +153,10 @@ function doStep() {
 		for (let j = 0; j <= size; j++) {
 			// RULES OF THE GAME OF LIFE
 			if (cellArray[i][j] === 0 && getNeighbour(i, j) === 3) {
+				// New cell comes to life
 				cellArray[i][j] = 1;
-			}
-			if (!(cellArray[i][j] === 1 &&
-				(getNeighbour(i, j) === 2 || getNeighbour(i, j) === 3))) {
+			} else if ((cellArray[i][j] === 1 &&
+				(getNeighbour(i, j) < 2 || getNeighbour(i, j) > 3))) {
 				cellArray[i][j] = 0;
 			}
 		}
